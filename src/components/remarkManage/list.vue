e68c8d53e7aa6c8f445d6c4e1f1214996325d91c
wanghuipeng <624793769@qq.com>
2018-09-02 23:33:38 +0800

    删除冗余

--- src/components/remarkManage/list.vue
+++ src/components/remarkManage/list.vue
@@ -1,16 +1,14 @@
 <template>
     <div class="main orderManage">
-        <blog-header />
         <blog-table />
      </div>
 </template>
 
 <script>
-import blogHeader from '@/components/remarkManage/header'
 import blogTable from '@/components/remarkManage/table'
 
 export default {
-  components: { blogHeader, blogTable },
+  components: { blogTable },
   data () {
     return {
     }
